3c9eea1e80f293db790f44a09e655c582d1bd04b
blackduck007 <blackduck897@gmail.com>
2021-09-17 23:53:50 +0530

    #81: added github api with calendar

---
+++ src/api/github-api.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+const GitHubCalendarApi = () => {
+    return (
+        <GitHubCalendar username="tmtanzeel" blockSize={10} blockMargin={4} />
+    );
+}
+
+export default GitHubCalendarApi;
